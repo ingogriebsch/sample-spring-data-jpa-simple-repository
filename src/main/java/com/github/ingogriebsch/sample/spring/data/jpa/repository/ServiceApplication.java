ea83a5c509982d094872b899f4abedcb688898c8
Ingo Griebsch <ingo.griebsch@gmx.de>
2020-12-17 20:30:09 +0100

    refactor: rename class Application to ServiceApplication

--- src/main/java/com/github/ingogriebsch/sample/spring/data/jpa/repository/ServiceApplication.java
+++ src/main/java/com/github/ingogriebsch/sample/spring/data/jpa/repository/ServiceApplication.java
@@ -20,9 +20,9 @@ import static org.springframework.boot.SpringApplication.run;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ServiceApplication {
 
     public static void main(String[] args) {
-        run(Application.class, args);
+        run(ServiceApplication.class, args);
     }
 }
